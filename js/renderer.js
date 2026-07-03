1878b2c76beb4e39b9222723295b82b77f2cf4a2
kanyfgk <nerimanaba1993@gmail.com>
2026-07-03 23:48:00 +0300

    Navigation Uses Components

--- js/renderer.js
+++ js/renderer.js
@@ -46,29 +46,7 @@ root.innerHTML = `
 
     </section>
 
-    <nav class="bottom-nav">
-
-        <button class="nav-btn active">
-            <div class="nav-icon">⌂</div>
-            Home
-        </button>
-
-        <button class="nav-btn">
-            <div class="nav-icon">ID</div>
-            Identity
-        </button>
-
-        <button class="nav-btn">
-            <div class="nav-icon">＋</div>
-            Create
-        </button>
-
-        <button class="nav-btn">
-            <div class="nav-icon">◌</div>
-            World
-        </button>
-
-    </nav>
+    ${components.navigation()}
 
 </main>
 `;
