878e6b008ad4a85e808b2d285fd7c6a661339384
kanyfgk <nerimanaba1993@gmail.com>
2026-07-03 21:59:57 +0300

    Create EventBus and Registry

--- js/core.js
+++ js/core.js
@@ -3,14 +3,41 @@ const VAERO = {
     version: "1.0",
 
     engine: null,
-
     renderer: null,
 
-    organs: {},
+    registry: {},
+
+    events: {},
 
     register(name, object){
 
-        this.organs[name] = object;
+        this.registry[name] = object;
+
+    },
+
+    get(name){
+
+        return this.registry[name] || null;
+
+    },
+
+    on(eventName, callback){
+
+        if(!this.events[eventName]){
+            this.events[eventName] = [];
+        }
+
+        this.events[eventName].push(callback);
+
+    },
+
+    emit(eventName, payload){
+
+        const listeners = this.events[eventName] || [];
+
+        listeners.forEach(callback => {
+            callback(payload);
+        });
 
     }
 
