28244fd35dba9336ab10fb3cd5e6a35dcbd7133f
kanyfgk <nerimanaba1993@gmail.com>
2026-07-03 21:42:22 +0300

    Upgrade Entity System

--- js/entity.js
+++ js/entity.js
@@ -1,14 +1,57 @@
 class Entity{
 
-    constructor(data={}){
+    constructor(data = {}){
 
         this.id = data.id || crypto.randomUUID();
 
         this.type = data.type || "entity";
 
-        this.name = data.name || "Unknown";
+        this.name = data.name || "Unnamed Entity";
 
-        this.createdAt = Date.now();
+        this.description = data.description || "";
+
+        this.status = data.status || "active";
+
+        this.organs = data.organs || [];
+
+        this.bridges = data.bridges || [];
+
+        this.createdAt = data.createdAt || Date.now();
+
+    }
+
+    addOrgan(organ){
+
+        if(!organ || !organ.name){
+            return;
+        }
+
+        this.organs.push(organ);
+
+    }
+
+    addBridge(bridge){
+
+        if(!bridge || !bridge.id){
+            return;
+        }
+
+        this.bridges.push(bridge);
+
+    }
+
+    toJSON(){
+
+        return {
+            id:this.id,
+            type:this.type,
+            name:this.name,
+            description:this.description,
+            status:this.status,
+            organs:this.organs,
+            bridges:this.bridges,
+            createdAt:this.createdAt
+        };
 
     }
 
