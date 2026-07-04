d883bb91ff0a305c74c2332de8ee9dcd1a2b9a8b
kanyfgk <nerimanaba1993@gmail.com>
2026-07-04 21:20:28 +0300

    Update bridge.js

--- js/bridge.js
+++ js/bridge.js
@@ -2,9 +2,26 @@ const Bridge = {
 
     links: [],
 
+    boot(){
+
+        const events = VAERO.get("events");
+
+        events.on("entity.mounted", (data)=>{
+
+            this.connect(
+                data.entityId,
+                "vaero-community",
+                "root-community"
+            );
+
+        });
+
+    },
+
     connect(from, to, type = "default"){
 
         const bridge = {
+            id: crypto.randomUUID(),
             from,
             to,
             type,
@@ -13,16 +30,28 @@ const Bridge = {
 
         this.links.push(bridge);
 
-        VAERO.emit("bridge:created", bridge);
+        VAERO.emit("bridge.created", bridge);
 
         return bridge;
+
+    },
+
+    remove(id){
+
+        this.links = this.links.filter(link => link.id !== id);
+
+    },
+
+    find(from){
+
+        return this.links.filter(link => link.from === from);
 
     },
 
     all(){
 
         return this.links;
- 
+
     }
 
 };
