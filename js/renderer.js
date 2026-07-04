3bd4f4c266b0c61fb13531d2187637a3c6aa6e6b
kanyfgk <nerimanaba1993@gmail.com>
2026-07-04 14:33:50 +0300

    Render Brain Status

--- js/renderer.js
+++ js/renderer.js
@@ -33,6 +33,7 @@ root.innerHTML = `
         ${components.bridge()}
         ${components.memory()}
         ${components.guardian()}
+        ${components.brain()}
 
         <div style="display:flex;gap:14px;margin-top:32px;">
 
