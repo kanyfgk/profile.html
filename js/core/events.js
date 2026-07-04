82f7de7561e621b4d3c9bd1b22afc26b19be95e0
kanyfgk <nerimanaba1993@gmail.com>
2026-07-04 18:12:51 +0300

    Update events.js

--- js/core/events.js
+++ js/core/events.js
@@ -2,6 +2,7 @@ class EventSystem {
 
     constructor() {
         this.events = {};
+        console.log("VAERO EventSystem Ready");
     }
 
     on(name, callback) {
@@ -12,11 +13,18 @@ class EventSystem {
 
         this.events[name].push(callback);
 
+        console.log("Event listener added:", name);
+
     }
 
     emit(name, data = {}) {
 
-        if (!this.events[name]) return;
+        console.log("Event emitted:", name, data);
+
+        if (!this.events[name]) {
+            console.warn("No listeners for event:", name);
+            return;
+        }
 
         this.events[name].forEach(callback => callback(data));
 
