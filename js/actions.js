9ef9bdad2383f30f3a3faa418bfb5a47bd36a5b1
kanyfgk <nerimanaba1993@gmail.com>
2026-07-07 00:45:11 +0300

    Update actions.js

--- js/actions.js
+++ js/actions.js
@@ -181,11 +181,6 @@ document.addEventListener("click", event => {
     VAERO.engine.currentEntityPage = "profile";
     VAERO.engine.mount(VAERO.engine.currentEntity);
 }
-
- case "entity:identity":
-    VAERO.engine.currentEntityPage = "identity";
-    VAERO.render();
-    break;
  
 if (action === "entity:organs") {
     VAERO.engine.currentEntityPage = "organs";
