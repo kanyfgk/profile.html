ab9aaaf9e0289322a6448f5f9eafd51a362b451f
kanyfgk <nerimanaba1993@gmail.com>
2026-07-07 15:06:02 +0300

    Update bridge-app.js

--- js/apps/bridge-app.js
+++ js/apps/bridge-app.js
@@ -1,6 +1,7 @@
 const BridgeApp = {
 
     render(entity){
+        VAERO.get("brainAwareness").enter("bridge");
 
         return `
             <div class="section" style="margin-top:24px;padding:24px;">
