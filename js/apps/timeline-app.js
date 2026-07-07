8915b9e90100a2516b6aa5c795cf8fab9511644d
kanyfgk <nerimanaba1993@gmail.com>
2026-07-07 15:05:21 +0300

    Update timeline-app.js

--- js/apps/timeline-app.js
+++ js/apps/timeline-app.js
@@ -1,6 +1,7 @@
 const TimelineApp = {
 
     render(entity){
+        VAERO.get("brainAwareness").enter("timeline");
 
         return `
             <div class="section" style="margin-top:24px;padding:24px;">
