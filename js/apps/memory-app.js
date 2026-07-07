f335e3ce8d27a681c37c3f3dde8c52b7dd9de04e
kanyfgk <nerimanaba1993@gmail.com>
2026-07-07 15:04:42 +0300

    Update memory-app.js

--- js/apps/memory-app.js
+++ js/apps/memory-app.js
@@ -1,6 +1,7 @@
 const MemoryApp = {
 
     render(entity){
+        VAERO.get("brainAwareness").enter("memory");
 
         return `
             <div class="section" style="margin-top:24px;padding:24px;"> 
