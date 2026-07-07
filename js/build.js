b10d7a5ca1ec8042fdba8cd42f1e7d76ff31820b
kanyfgk <nerimanaba1993@gmail.com>
2026-07-07 21:46:38 +0300

    Create build.js

---
+++ js/build.js
@@ -0,0 +1,9 @@
+const VAERO_BUILD = {
+    id: "build-001",
+    date: "2026-07-07",
+    note: "deploy verification active"
+};
+
+window.VAERO_BUILD = VAERO_BUILD;
+
+console.log("VAERO BUILD:", VAERO_BUILD.id, VAERO_BUILD.date, VAERO_BUILD.note);
