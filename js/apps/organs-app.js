9f5b797fa71917f2f28eb0cc6288d5ed9773861e
kanyfgk <nerimanaba1993@gmail.com>
2026-07-08 19:01:03 +0300

    Fix Brain mobile button

--- js/apps/organs-app.js
+++ js/apps/organs-app.js
@@ -64,13 +64,13 @@ const OrgansApp = {
                     data-action="brain:open"
                     style="
                         position:fixed;
-                        right:24px;
-                        bottom:24px;
+                        right:18px;
+                        bottom:110px;
                         width:62px;
                         height:62px;
                         border-radius:50%;
                         font-size:28px;
-                        z-index:999;
+                        z-index:99999;
                     ">
                     ✨
                 </button>
